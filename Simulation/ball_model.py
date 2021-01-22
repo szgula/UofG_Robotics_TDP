aacaab0822523075315c8329abac85ba173e42f2
szgula <szymon.gula@icloud.com>
2021-01-22 16:47:30 +0000

    First iteration of the ball with full functionality + functionality tested

--- Simulation/ball_model.py
+++ Simulation/ball_model.py
@@ -83,6 +83,7 @@ class BallBasicModel(BallModel):
 
     def _wall_collision(self, collision_object, wall_orientation: str, **kwargs):
         """
+        Ball bounce from the static wall - can be horizontal or vertical
         :param collision_object:
         :param wall_orientation: "vertical", "horizontal", "corner"
         :param kwargs:
@@ -94,11 +95,15 @@ class BallBasicModel(BallModel):
             self._y_vel *= -1 * self._vel_bounce_coefficient
 
     def _elastic_collision_with_round_player(self, collision_object):
-        """ NOT VERIFIED """
+        """
+        Ball bounce from the round player, the ball diameter is assumed 0, the ball rotation effect is mitigated
+        :param collision_object:
+        :return:
+        """
         player_pos_x, player_pos_y = collision_object.get_position_components_wcs()
         player_radius = collision_object.radius
         dx, dy = self._x_pos - player_pos_x, self._y_pos - player_pos_y
-        diff = (dx ** 2 + dy ** 2) ** 0.5
+        diff = (dx ** 2 + dy ** 2) ** 0.5  # TODO: add check if in collision range (of this time step)
         player_vx, player_vy = collision_object.get_velocity_components_wcs()
 
         """ Calculate intersection point between player and ball - assuming the ball radius ~= 0 for simplicity 
@@ -112,50 +117,52 @@ class BallBasicModel(BallModel):
         move_vel_threshold = 0.001              # TODO: find a better place for this
         if self._x_vel >= move_vel_threshold:
             # solving ax^2 + bx + c = 0
-            a = (1 + self._y_vel**2 / self._x_vel**2)
-            M = (self._y_vel / self._x_vel) * self._x_pos - self._y_pos + player_pos_y
-            b = -2*(player_pos_x - M * (self._y_vel / self._x_vel))
+            k = (self._y_vel / self._x_vel)
+            M = k * self._x_pos - self._y_pos + player_pos_y
+            a = (1 + k ** 2)
+            b = -2*(player_pos_x + M * k)
             c = player_pos_x**2 + M**2 - player_radius**2
         else:
             a = 1
-            b = 2 * player_pos_y
-            c = player_pos_y**2 + (self._x_pos - player_pos_y)**2 - player_radius**2
+            b = -2 * player_pos_y
+            c = player_pos_y**2 + (self._x_pos - player_pos_x)**2 - player_radius**2
 
         #  solve quadratic equation
         delta = b ** 2 - 4 * a * c
         if delta < 0:
             return
         delta_root = delta ** 0.5
-        x1, x2 = (-b + delta_root) / (2 * a), (-b - delta_root) / (2 * a)
-        get_y = lambda _x_: self._y_pos + self._y_vel * (_x_ - self._x_pos) / self._x_vel
-        y1, y2 = get_y(x1), get_y(x2)
+        sol1, sol2 =  (-b + delta_root) / (2 * a), (-b - delta_root) / (2 * a)
+        if self._x_vel >= move_vel_threshold:
+            x1, x2 = sol1, sol2
+            get_y = lambda _x_: self._y_pos + self._y_vel * (_x_ - self._x_pos) / self._x_vel
+            y1, y2 = get_y(x1), get_y(x2)
+        else:
+            x1, x2 = self._x_pos, self._x_pos
+            y1, y2 = sol1, sol2
 
         dist_1 = (x1 - self._x_pos)**2 + (y1 - self._y_pos)**2
         dist_2 = (x2 - self._x_pos)**2 + (y2 - self._y_pos)**2
-        #  get player-ball collision point
-        collision_x, collision_y = (x1, y1) if dist_1 < dist_2 else (x2, y2)
+        collision_x, collision_y = (x1, y1) if dist_1 < dist_2 else (x2, y2)  #  get player-ball collision point
 
         # https://math.stackexchange.com/questions/2239169/reflecting-a-vector-over-another-line
         incoming_vector = (self._x_vel, self._y_vel)
-        perpendicular_mirror_vector = (collision_x - player_pos_x, collision_y - player_pos_y)
-        m_x, m_y = perpendicular_mirror_vector
-        # FIXME: not working for m_y = 0 - situation when ball moves along x axis
-        mirror_vector = (1, -m_x / m_y)
-        n_x, n_y = mirror_vector
+        m_x, m_y = incoming_vector
+        normal_reflection_vector = (collision_x - player_pos_x, collision_y - player_pos_y)
+        n_x, n_y = normal_reflection_vector
 
-        r_x = m_x - 2 * (m_x * n_x + m_y * n_y) / (n_x * n_x + n_y * n_y) * n_x
-        r_y = m_y - 2 * (m_x * n_x + m_y * n_y) / (n_x * n_x + n_y * n_y) * n_y
+        # scalar product of (incoming_vector * normal_reflection_vector) / (normal_reflection_vector * normal_reflection_vector)
+        sp = (m_x * n_x + m_y * n_y) / (n_x * n_x + n_y * n_y)
+        r_x = m_x - 2 * sp * n_x
+        r_y = m_y - 2 * sp * n_y
 
         vel_ = (self._x_vel**2 + self._y_vel**2)**0.5
         ref_vel = (r_x**2 + r_y**2)**0.5
-
         # TODO: vel_ *= self._vel_bounce_coefficient
-
         vel_x = player_vx + r_x * vel_ / ref_vel
         vel_y = player_vy + r_y * vel_ / ref_vel
         self._x_vel = vel_x
         self._y_vel = vel_y
-
 
     def _kick_collision(self, collision_object, kick_vel: float = None, **kwargs):
         """
@@ -187,6 +194,7 @@ class BallBasicModel(BallModel):
         return self._x_pos, self._y_pos
 
 
+# TODO: move to separate test file and integrate it with pytest or other testing framework
 class BallTests:
 
     @staticmethod
@@ -335,7 +343,7 @@ class BallTests:
                                          player_vel_x=0, player_vel_y=0,
                                          ball_pos_x=0, ball_pos_y=0,
                                          ball_vel_x=0, ball_vel_y=5,
-                                         player_radius=0.1):
+                                         player_radius=0.1, dt=0.01, time=100):
         import matplotlib.pyplot as plt
 
         class DummpPlayer:
@@ -356,8 +364,8 @@ class BallTests:
                 return diff <= self.r_sq * 4 # add some threshold for leg or sth
 
         player = DummpPlayer(player_pos_x, player_pos_y, player_vel_x, player_vel_y, player_radius)
-        steps, kick_range = 1000, 0.2
-        ball = BallBasicModel(init_x_pos=ball_pos_x, init_y_pos=ball_pos_y)
+        steps, event_index = int(time/dt), int(0.5*time/dt)
+        ball = BallBasicModel(init_x_pos=ball_pos_x, init_y_pos=ball_pos_y, dt=dt)
         ball._x_vel, ball._y_vel = ball_vel_x, ball_vel_y
         history = {'x': [], 'y': [], 'dx': [], 'dy': [], 't': []}
         collide = False
@@ -367,32 +375,49 @@ class BallTests:
                 ball._elastic_collision_with_round_player(player)
                 collide = True
             x, y = ball.get_position()
-            history['x'].append(x), history['y'].append(y), history['t'].append(i)
+            history['x'].append(x), history['y'].append(y), history['t'].append(i*dt)
             history['dx'].append(ball._x_vel), history['dy'].append(ball._y_vel)
 
         f, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        f.suptitle(f"test_collision_with_round_player: initial conditions: \nplayer (x, y, x', y', size): {player_pos_x, player_pos_y,player_vel_x, player_vel_y, player_radius} \nball (x, y, x', y'): {ball_pos_x, ball_pos_y,ball_vel_x, ball_vel_y}, \ndt={dt}, time={time} ")
         ax1.plot(history['x'], history['y'], label='pos')
         ax1.scatter(player_pos_x, player_pos_y, label='player')
+        ax1.set_xlabel('x'), ax1.set_xlabel('y')
         ax1.legend()
         ax2.plot(history['t'], history['x'], label='x_pos(t)')
         ax2.plot(history['t'], history['y'], label='y_pos(t)')
+        ax2.set_xlabel('t'), ax1.set_xlabel('x & y')
         ax2.legend()
         ax3.plot(history['t'], history['dx'], label='x_vel(t)')
         ax3.plot(history['t'], history['dy'], label='y_vel(t)')
+        ax3.set_xlabel('t'), ax1.set_xlabel('x & y')
         ax3.legend()
         return True
 
 
 if __name__ == "__main__":
-    #BallTests.test_ball_stationary()
-    #BallTests.test_ball_moving_with_initial_speed(2,1)
-    #BallTests.test_ball_wall_bouncing("vertical")
-    #BallTests.test_ball_wall_bouncing("horizontal")
-    #BallTests.test_ball_wall_bouncing("corner")
-    #BallTests.test_kick(-1, 0)  # player on the left of the ball -> kick East
-    #BallTests.test_kick(0, 1)  # player above the ball -> kick South
-    #BallTests.test_kick(1, -1)  # player right and below the ball -> kick North-West
-    #BallTests.test_kick(1, -3)  # player right and below the ball -> kick North-West
-    #BallTests.test_receive()
-    BallTests.test_collision_with_round_player()
+    BallTests.test_ball_stationary()
+    BallTests.test_ball_moving_with_initial_speed(2,1)
+    BallTests.test_ball_wall_bouncing("vertical")
+    BallTests.test_ball_wall_bouncing("horizontal")
+    BallTests.test_ball_wall_bouncing("corner")
+    BallTests.test_kick(-1, 0)  # player on the left of the ball -> kick East
+    BallTests.test_kick(0, 1)  # player above the ball -> kick South
+    BallTests.test_kick(1, -1)  # player right and below the ball -> kick North-West
+    BallTests.test_kick(1, -3)  # player right and below the ball -> kick North-West
+    BallTests.test_receive()
+    BallTests.test_collision_with_round_player(player_pos_x=0, player_pos_y=10, player_vel_x=0, player_vel_y=0,
+                                               ball_pos_x=0, ball_pos_y=0, ball_vel_x=0, ball_vel_y=5,)
+    BallTests.test_collision_with_round_player(player_pos_x=10, player_pos_y=0, player_vel_x=0, player_vel_y=0,
+                                               ball_pos_x=0, ball_pos_y=0, ball_vel_x=5, ball_vel_y=0, )
+    BallTests.test_collision_with_round_player(player_pos_x=10, player_pos_y=-10, player_vel_x=0, player_vel_y=0,
+                                               ball_pos_x=0, ball_pos_y=0, ball_vel_x=5, ball_vel_y=-5, dt=0.001)
+    BallTests.test_collision_with_round_player(player_pos_x=10, player_pos_y=-10.1, player_vel_x=0, player_vel_y=0,
+                                               ball_pos_x=0, ball_pos_y=0, ball_vel_x=5, ball_vel_y=-5, dt=0.001)
+    BallTests.test_collision_with_round_player(player_pos_x=10, player_pos_y=-9.9, player_vel_x=0, player_vel_y=0,
+                                               ball_pos_x=0, ball_pos_y=0, ball_vel_x=5, ball_vel_y=-5, dt=0.001)
+    BallTests.test_collision_with_round_player(player_pos_x=6, player_pos_y=9.9, player_vel_x=0, player_vel_y=0,
+                                               ball_pos_x=0, ball_pos_y=0, ball_vel_x=3, ball_vel_y=5, dt=0.001)
+
+    # TODO: add tests for the moving player
 
