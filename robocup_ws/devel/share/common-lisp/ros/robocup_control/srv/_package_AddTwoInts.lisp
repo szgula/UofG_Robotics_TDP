03921fab5cd634d79af7b367bac6d27734f9674d
Omar Jabri <omarjabri967@gmail.com>
2021-02-05 11:08:56 -0800

    Created structured project with python files integrated within ros file system, changed imports in python files to suit new file system structure.

--- robocup_ws/devel/share/common-lisp/ros/robocup_control/srv/_package_AddTwoInts.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package robocup_control-srv)
-(cl:export '(A-VAL
-          A
-          B-VAL
-          B
-          SUM-VAL
-          SUM
-))
--- robocup_ws/devel/share/robocup_control/cmake/robocup_control-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(robocup_control_MSG_INCLUDE_DIRS "")
-set(robocup_control_MSG_DEPENDENCIES std_msgs)
