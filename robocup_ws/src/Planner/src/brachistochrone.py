44d333ea1c4b6cf10d9c26ffc18abcb402916553
OmarJabri7 <omarjabri967@gmail.com>
2021-02-22 12:49:45 +0000

    added solution for finding fastest time between point A and B alongside log file to output important information

---
+++ robocup_ws/src/Planner/src/brachistochrone.py
@@ -0,0 +1,67 @@
+import logging
+import numpy as np
+from sympy import Symbol
+from sympy.solvers import solve
+from scipy.optimize import newton
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+from sympy import lambdify
+import sympy
+
+logging.basicConfig(filename="log_paths.txt",
+                            filemode='a',
+                            format='%(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
+
+def cycloid(x0, y0, x_t, y_t, V, N = 6):
+    def f(theta):
+        return y_t / x_t - (1 - np.cos(theta)) / (theta - np.sin(theta))
+    circle_theta = newton(f,np.pi/2,maxiter=1000)
+    # circle_theta = newton(f,np.pi/2)
+    # circle_theta = fsolve(f,np.pi/2)
+    R = y_t / (1 - np.cos(circle_theta))
+    angle = Symbol('angle')
+    eq_x = lambdify(angle,R * (angle - sympy.sin(angle)) - x0)
+    eq_y = lambdify(angle,R * (1 - sympy.cos(angle)) - y0)
+    solution_x = fsolve(eq_x,np.pi/2)
+    solution_y = fsolve(eq_y,np.pi/2)
+    theta_x = np.linspace(solution_x, circle_theta, N)
+    theta_y = np.linspace(solution_y, circle_theta, N)
+    x = R * (theta_x - np.sin(theta_x))
+    y = R * (1 - np.cos(theta_y))
+    T = circle_theta * np.sqrt(R / g)
+    Vx = V*np.cos(theta_x)
+    Vy = V*np.sin(theta_y)
+    print(f"{Vx}, {Vy}")
+    return x, y, T, theta_x, theta_y
+
+g = 9.81
+trajectory = np.array([[1.5, 1.2],
+                       [2.5, 3],
+                       [4, 7.5],
+                       [10, 8]])
+fig, ax = plt.subplots()
+path = []
+time = []
+displacement = []
+init_x = 0.5
+init_y = 0.5
+V = 2
+logging.info("#######################")
+for point in trajectory:
+    x, y, T, theta_x, theta_y = cycloid(init_x,
+                      init_y,
+                      point[0],
+                      point[1], V)
+    path.append([x,y])
+    time.append(T)
+    # print(theta_x.reshape([1,6]))
+    # displacement.append(theta_dot)
+    logging.info(f"({init_x}, {init_y}) => ({point[0]}, {point[1]})")
+    logging.info(f"x: {x}")
+    logging.info(f"y: {y}")
+    # logging.info(f"Displacement: {theta_dot}")
+    logging.info(f"Time taken: {T} seconds")
+    init_x += 0.5
+    init_y += 0.5
