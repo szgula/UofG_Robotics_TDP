2c8f1494f7dfab13fbdb07d2b2ae0439e40a9900
Omar Jabri <omarjabri967@gmail.com>
2021-03-07 12:35:17 -0800

    Updated strategy with defence and strikers to follow a structured approach of the game + passing. TODO: shooting!

--- robocup_ws/src/Planner/src/decision_tree.py
+++ robocup_ws/src/Planner/src/decision_tree.py
@@ -13,37 +13,45 @@ class DecisionTree():
     def __init__(self,team_id, pc = None):
         self.threshold = 0.1
         self.controller = pc
+        self.field_size = [10,6]
         self.team_id = team_id
         self.net_pos_x_wcs = 5 if self.team_id == 1 else -5
         self.net_pos_y = TeamMasterSupporting.net_length / 2 - 0.1 if self.team_id == 1 \
             else -TeamMasterSupporting.net_length / 2 + 0.1
         self.net_coords = [self.net_pos_x_wcs, self.net_pos_y]
+        self.enemy_net = [5, TeamMasterSupporting.net_length / 2 - 0.1]
 
     def plan(self, team: list, enemy: list, player_id: int, mode: str):
         game_info = [team, enemy, player_id, mode]
 
         if(self.controller.has_ball(game_info)):
-            
+            print("HAS BALL")
             if(self.controller.if_able_score(game_info)):
                 self.controller.score(game_info)
             else:
-                can_pass, pass_candidate = self.controller.check_for_pass(game_info)
+                can_pass, pass_candidate = self.controller.check_for_pass(game_info, self.enemy_net)
                 if(can_pass):
+                    # is_at_strategic_point = self.controller.check_pass_candidate_pos(self.strategic_point, pass_candidate)
                     return self.controller.pass_ball(game_info,pass_candidate)
                 else:
                     self.controller.dribble(game_info) #TODO DRIBBLE PLAYER-BALL
         else:
             closest_player = self.controller.closest_to_ball(game_info)
-            ball_is_free, enemy_id = self.controller.ball_is_free(game_info)
+            #if team_or_enemy = 1 => ball with teammate else ball with opponent
+            ball_is_free ,enemy_id = self.controller.ball_is_free(game_info)
             take_turn = np.random.randint(0,2) #Add some random activity
-            print(take_turn)                         
-            if(closest_player == player_id or mode == "ATTACK"):
+            #STRIKERS
+            if(closest_player == player_id and mode == "ATTACK"):
                 return self.controller.go_to_ball(game_info)
+            elif(closest_player != player_id and mode == "ATTACK"):
+                return self.controller.go_to_strategic_point(game_info, closest_player)
+            #DEFENDERS
+            elif(closest_player == player_id and ball_is_free and mode == "DEFEND"):
+                return self.controller.go_to_ball(game_info) #TODO: go to defence position
             elif(closest_player != player_id and not ball_is_free and mode == "DEFEND"):
-                # if(take_turn == 1):
-                return self.controller.intercept(game_info, enemy_id, self.net_coords)
-                # else:
-                #     return self.controller.go_to_strategic_point(game_info)
+                return self.controller.intercept(game_info, enemy_id, self.net_coords) #COVER OVER DEFENCE
+            # elif(closest_player != player_id and mode == "DEFEND"):
+            #     return self.controller.intercept(game_info, enemy_id, self.net_coords)  # COVER OVER DEFENCE
             else:
-                return self.controller.go_to_strategic_point(game_info)
+                return PlayerCommand(0,0,0)
 
--- robocup_ws/src/Planner/src/player_controller.py
+++ robocup_ws/src/Planner/src/player_controller.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from BasicCommonActions.go_to_action import simple_go_to_action
 from game_interfaces.msg import Position
 from brachistochrone import cycloid
-from BasicCommonActions.go_to_action import go_to_fast
+from BasicCommonActions.go_to_action import go_to_fast, receive_and_pass_action
 from BasicCommonActions.plan_supporting_functions import TeamMasterSupporting
 import math
 
@@ -19,7 +19,7 @@ class PlayerController:  #(Robot)
         self.player = player_id
         self.points_to_visit = []
         self.current_goal = 0
-        self.goal_threshold = 0.1
+        self.goal_threshold = 0.2
         self.intercept_threshold = 0.2
 
     def has_ball(self, game_info: list) -> bool:
@@ -28,40 +28,30 @@ class PlayerController:  #(Robot)
         position = team.players_positions_efcs[player_id]
         ball_pos = team.ball_pos_efcs
         d = np.hypot(ball_pos.x - position.x, ball_pos.y - position.y)
+        # print(f"DISTANCE: {d}")
         if (d <= self.goal_threshold):
             return True
         return False
 
     def if_able_score(self, game_info: list):
-        pass
+        return False #TODO
 
-    def check_for_pass(self, game_info: list) -> [bool, int]:
+    def check_for_pass(self, game_info: list, net:list) -> [bool, int]:
         team = game_info[0]
         player_id = game_info[2]
         positions = team.players_positions_efcs
-        position = [[position.x, position.y] for position in positions]
-        main_player = position[player_id]
-        position[player_id] = [np.inf, np.inf]
-        position = np.array(position).astype(float)
-        player_pos = np.array([main_player[0], main_player[1]]).astype(float)
-        idx = np.argmin(np.linalg.norm(position - player_pos, axis=1))
-        return True, idx
+        pass_candidate = self.get_team_pass_candidate(positions, player_id, net)
+        return True, pass_candidate
 
     def pass_ball(self, game_info: list, candidate: int) -> PlayerCommand:
         team = game_info[0]
+        ball_vel = team.ball_vel_efcs
         main_player = team.players_positions_efcs[game_info[2]]
         candidate_pos = team.players_positions_efcs[candidate]
         position = np.array([candidate_pos.x,candidate_pos.y])
         ball = team.ball_pos_efcs
-        ball_pos = np.array([ball.x, ball.y])
-        relative_vector = position - ball_pos
-        unit_vector = relative_vector/np.linalg.norm(relative_vector)
-        vantage_point = ball_pos - 0.1*unit_vector
-        kick = 1
-        if(np.linalg.norm(np.array([main_player.x, main_player.y]).astype(float) - vantage_point) <= 0.1):
-            return PlayerCommand(0,0,kick)
-        lv, rv = go_to_fast(main_player,Position(vantage_point[0],vantage_point[1],0))
-        return PlayerCommand(lv,rv,0)
+        lv, rv, action = receive_and_pass_action(main_player,candidate_pos,ball, ball_vel)
+        return PlayerCommand(lv,rv,action)
 
 
     def closest_to_ball(self,game_info: list) -> int:
@@ -73,9 +63,24 @@ class PlayerController:  #(Robot)
         ball_pos = np.array([team.ball_pos_efcs.x,team.ball_pos_efcs.y]).astype(float)
         idx = np.argmin(np.linalg.norm(position-ball_pos, axis=1))
         return idx
+#This function allows a striker to follow his teammate who is closest or has the ball in a parallel manner
+    def go_to_strategic_point(self,game_info, partner_id):
+        team = game_info[0]
+        player_id = game_info[2]
+        ball_pos = team.ball_pos_efcs
+        team_positions = team.players_positions_efcs
+        main_player = team_positions[player_id]
+        partner_player = team_positions[partner_id]
+        strategic_point = partner_player
+        strategic_point.x = float(strategic_point.x) + 1
+        if(partner_player.y < 0):
+            strategic_point.y = float(strategic_point.y) +  1
+        else:
+            strategic_point.y = float(strategic_point.y) - 1
+        lv, rv = go_to_fast(main_player, strategic_point)
+        return PlayerCommand(lv,rv,0) #TODO
 
-    def go_to_strategic_point(self,game_info):
-        return PlayerCommand(0,0,0) #TODO
+    # def check_pass_candidate(self,game_info, :
 
     def intercept(self,game_info: list, enemy_id: int, net: list) -> PlayerCommand:
         team = game_info[0]
@@ -97,13 +102,20 @@ class PlayerController:  #(Robot)
         lv, rv = go_to_fast(main_player,Position(danger_corner_1[0],danger_corner_1[1],0))
         return PlayerCommand(lv,rv,0)
 
+    def get_team_pass_candidate(self, team: list, player_id: int, net: list) -> int:
+        position = [np.array([position.x, position.y]) for position in team]
+        position[player_id] = np.array([np.inf, np.inf])
+        team_pass_candidate = np.argmin(np.linalg.norm(position - np.array(net), axis=1))
+        print(team_pass_candidate)
+        return team_pass_candidate
+
     def get_opponent_pass_candidate(self, enemies: list, enemy_id: int, net: list) -> int:
         position = [np.array([position.x, position.y]) for position in enemies]
         position[enemy_id] = np.array([np.inf, np.inf])
         opponent_pass_candidate = np.argmin(np.linalg.norm(position - np.array(net), axis=1))
         return opponent_pass_candidate
 
-    def ball_is_free(self,game_info: list) -> [bool, int]:
+    def ball_is_free(self,game_info: list) -> [bool, int, int]:
         team = game_info[0]
         opponents = game_info[1]
         positions = opponents.players_positions_wcs
@@ -112,7 +124,6 @@ class PlayerController:  #(Robot)
         ball_pos = np.array([team.ball_pos_efcs.x, team.ball_pos_efcs.y]).astype(float)
         min_pos = np.min(np.linalg.norm(position - ball_pos, axis=1))
         min_arg = np.argmin(np.linalg.norm(position - ball_pos, axis=1))
-        print(min_pos)
         if(min_pos <= self.intercept_threshold):
             return False, min_arg
         return True, -1
